b03970c42943142d2322a2f5dd5843bf1ef228a4
BoLjshOJ <boljshoj@gmail.com>
2019-02-13 11:28:25 +0300

    added: -created User entity -created WebSecurityConfig -created UserRepo -created RegistrationController -created Login and Registration pages -Refactor Controllers -others minors refactoring

---
+++ src/main/java/com/boljshoj/Sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.boljshoj.Sweater.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/boljshoj/Sweater/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.boljshoj.Sweater.repos;
+
+import com.boljshoj.Sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
