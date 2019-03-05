96082b8521d4b297d2ce6c7715881b5b1b3a0bf6
BoLjshOJ <boljshoj@gmail.com>
2019-03-05 15:17:09 +0300

    1. Create page 'My Message' 2. Add ability to edit own messages 3. Minor updates some templates 4. Add db version with some users

---
+++ src/main/resources/db/migration/V4__Add_some_users.sql
@@ -0,0 +1,6 @@
+insert into usr (id, username , password, active)
+      values  (2, 'user1', '1', true), (3, 'user2', '2', true), (4, 'user3', '3', true);
+
+insert into user_role (user_id, roles)
+      values (2, 'USER'), (3, 'USER'), (4, 'USER');
+
